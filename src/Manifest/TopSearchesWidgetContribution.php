ee92dc6fc71d5a55524b266d50999ab562327b21
Ben <howdu@users.noreply.github.com>
2026-07-12 08:56:25 +0100

    fix: align packages with Capell v4 extension API

--- src/Manifest/TopSearchesWidgetContribution.php
+++ src/Manifest/TopSearchesWidgetContribution.php
@@ -11,6 +11,6 @@ final class TopSearchesWidgetContribution implements ExtensionContribution, Regi
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 }
